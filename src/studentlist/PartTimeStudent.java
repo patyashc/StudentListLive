43e92b0681e2c079ed8db026829e941f57a3a504
iamya <iamya@LAPTOP-DP8C1GK2>
2021-05-28 15:50:58 -0400

    new branch new java file

--- src/studentlist/PartTimeStudent.java
+++ src/studentlist/PartTimeStudent.java
@@ -1,13 +1,12 @@
-package studentlist;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package studentlist;
 
 /**
- * This class is for Git demo 
+ * This is the new class for Part Time Student demo 
  * @author iamya
  */
 public class PartTimeStudent {
